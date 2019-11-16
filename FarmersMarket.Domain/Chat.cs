794b01abbcfb57aa40c7d7084461d402a8c0663f
arkonq@gmail.com <arkonq@gmail.com>
2019-11-16 20:15:09 +0600

    Добавьте файлы проекта.

---
+++ FarmersMarket.Domain/Chat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FarmersMarket.Domain
+{
+	public class Chat : Entity
+	{
+		public Customer Seller { get; set; }
+		public Customer Customer { get; set; }
+		public virtual ICollection<Message> Messages { get; set; }
+	}
+}
---
+++ FarmersMarket.Domain/Seller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FarmersMarket.Domain
+{
+	public class Seller : Entity
+	{
+		public User User { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Address { get; set; }
+		public virtual ICollection<SellerProduct> Products { get; set; }
+		public virtual ICollection<Rating> Ratings { get; set; }
+		public virtual ICollection<Chat> Chats { get; set; }
+	}
+}
